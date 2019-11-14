4113a0671b6fa81c3cd481ee711e07a5e5884aca
ialexies <ialexies@gmail.com>
2019-11-14 23:32:19 +0800

    with firebase authentication using google

---
+++ lib/pages/home.dart
@@ -0,0 +1,115 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+final GoogleSignIn googleSignIn = GoogleSignIn();
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+ 
+  bool isAuth = false;
+
+  @override
+  void initState(){
+    super.initState();
+    googleSignIn.onCurrentUserChanged.listen((account){
+      handleSignIn(account);
+    }, onError: (err){
+      print('Error signing in: $err');
+    });
+    //Reauthenticate user when app is opened
+    googleSignIn.signInSilently(suppressErrors: false).then((account){
+      handleSignIn(account);
+    }).catchError((err){
+      print('Error signing in: $err');
+    });
+  }
+
+  handleSignIn(GoogleSignInAccount account ){
+    if(account!=null){
+        print('user Signed in! ${account.displayName}');
+        setState(() {
+          isAuth = true;
+        });
+      } else{
+         setState(() {
+           isAuth = false;
+         });
+      }
+  }
+
+  login(){
+    googleSignIn.signIn();
+  }
+
+  logout(){
+    googleSignIn.signOut();
+  }
+
+  Widget buildAuthScreen(){
+    return RaisedButton(
+      child: Text('Logout'),
+      onPressed: logout(),
+    );
+  }
+
+  Scaffold buildUnAuthScreen(){
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topRight,
+            end: Alignment.bottomLeft,
+            colors: [
+              Theme.of(context).primaryColor ,
+              Theme.of(context).accentColor,
+            ]
+          ),
+        ),
+        alignment: Alignment.center,
+        child: Column(
+        mainAxisAlignment: MainAxisAlignment.center ,
+        crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'DigitApps  Blog',
+              style: TextStyle(
+                fontFamily: "Signatra",
+                fontSize: 60.0,
+                color: Colors.white,
+              ),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            // to detect the gesture on the image. 
+            GestureDetector(
+              onTap: login(),
+              child: Container(
+                width: 260,
+                height: 60,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/images/google_signin_button.png'),
+                    fit: BoxFit.cover
+                  ),
+                ),
+              ),
+            ),
+          ], 
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context){
+    return  isAuth ?  buildAuthScreen() : buildUnAuthScreen();
+  }
+
+
+}
